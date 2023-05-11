e122856a94887cf23058257e0676d56131642b3b
thejabh <bhtheja2002@gamil.com>
2023-05-11 22:17:03 +0530

    adding all files commit

---
+++ homepage.js
@@ -0,0 +1,11 @@
+function homepage(){
+    location.replace("homepage.html")
+}
+
+function about_us(){
+    location.replace("about-us/about.html")
+}
+
+function login(){
+    location.replace("index.html")
+}
